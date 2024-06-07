694927644d8f87128677d3d1cc2a525c51918b7b
Kamily <costakamily3@gmail.com>
2024-06-07 09:13:08 -0300

    separa as telas em arquivos

---
+++ src/views/ContatoScreen.js
@@ -0,0 +1,23 @@
+import { View, Text, Button } from 'react-native';
+
+export default function ContatoScreen(props) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Contato Screen</Text>
+        <Button
+        title='Ir para Home'
+        onPress={() => props.navigation.navigate('Home')}
+      />
+       <Button
+        title='Ir para Sobre' any
+        onPress={() => props.navigation.navigate('Sobre')}
+      />
+       <Button
+        title='Ir para Produtos' any
+        onPress={() => props.navigation.navigate('Produtos')}
+      />
+      
+     
+      </View>
+    );
+  }
