7ea2b15b03748c240c195ad804263f52a9b3f546
Kamily <costakamily3@gmail.com>
2024-06-13 09:17:52 -0300

    Adiciona o paper ao projeto

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
  */
 
 import {AppRegistry} from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+//consegue usar isso em qualquer lugar do app
+export default props => {
+    return(
+        <PaperProvider>
+            <App/>
+        </PaperProvider>
+    )
+}
+
 
 AppRegistry.registerComponent(appName, () => App);
